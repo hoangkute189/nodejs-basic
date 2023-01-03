5f1628cb8d0576eb3d93be2eb8ab7e7ba0a5cf69
hoangkute189 <114124154+hoangkute189@users.noreply.github.com>
2023-01-03 21:37:00 +0700

    static file to use css/js/images

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public')) // trỏ đường dẫn đến file public (show các file static cho browser)
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config(); // import .env để lấy port
 
-const port = 8080
+const port = process.env.PORT || 8080; // phuong an backup underfine khi lỗi port 
 const app = express();
 
 configViewEngine(app);
