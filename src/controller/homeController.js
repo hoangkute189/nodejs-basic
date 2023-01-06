ecfa7a9434a792a1b861061c9f23cd1f61363b2e
hoangkute189 <114124154+hoangkute189@users.noreply.github.com>
2023-01-06 16:00:27 +0700

    request And use Database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,20 @@
+import connection from '../configs/connectDB';
+
 let getHomepage = (req, res) => {
     // logic
-    return res.render('index.ejs')
+    let data = [];
+    connection.query(
+    'SELECT * FROM `user` ',
+    function(err, results, fields) {
+        console.log('>>> Check my sql:')
+        // console.log(results);
+        data = results.map((row) => {
+            return row
+        })
+        console.log(data)
+        return res.render('index.ejs', {dataUser: JSON.stringify(data)})
+    });
+    
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web';
+// import connection from './configs/connectDB';
+
 require('dotenv').config(); // import .env để lấy port
 
 const port = process.env.PORT || 8080; // phuong an backup underfine khi lỗi port 
